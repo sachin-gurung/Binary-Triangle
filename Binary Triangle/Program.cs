9daca492ad27ce4ff25815af85ecdb4f2a036961
Sachin Gurung <saching061@hotmail.com>
2019-12-10 21:35:14 -0800

    First Commit

--- Binary Triangle/Program.cs
+++ Binary Triangle/Program.cs
@@ -1,15 +1,50 @@
-﻿using System;
+﻿/*
+ * C# Program to Print a BinaryTriangle
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Binary_Triangle
+namespace Program
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(String[] args)
         {
+            int p, lastInt = 0, input;
+            Console.WriteLine("Enter the Number of Rows : ");
+            input = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= input; i++)
+            {
+                for (p = 1; p <= i; p++)
+                {
+                    if (lastInt == 1)
+                    {
+                        Console.Write("0");
+                        lastInt = 0;
+                    }
+                    else if (lastInt == 0)
+                    {
+                        Console.Write("1");
+                        lastInt = 1;
+                    }
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
         }
     }
 }
+/*
+Enter the Number of Rows :
+6
+1
+01
+010
+1010
+10101
+010101
+
+Press any key to continue . . .
+*/
